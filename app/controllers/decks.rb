3fb0506f976a6a1d3d94a3bb2bf4d224cd6ce7df
Maggie Baker <maggiedbaker@gmail.com>
2015-10-03 14:54:37 -0400

    fixed duplicate decks/id

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -8,13 +8,13 @@ get '/decks/:id' do |id|
   erb :'decks/show'
 end
 
-get '/decks/:id' do |id|
-  @deck = Deck.find(id)
-  @cards = @deck.cards.dup
-  @current_card = @cards.sample
-  # redirect '/cards/#{current_card.id}'
-  erb :'cards/show'
-end
+# get '/decks/:id' do |id|
+#   @deck = Deck.find(id)
+#   @cards = @deck.cards.dup
+#   @current_card = @cards.sample
+#   # redirect '/cards/#{current_card.id}'
+#   erb :'cards/show'
+# end
 
 
 
