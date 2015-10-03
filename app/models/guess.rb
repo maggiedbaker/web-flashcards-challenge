2b61a133a76c7663f5e80d4e90956c481147c660
Maggie Baker <maggiedbaker@gmail.com>
2015-10-03 19:55:38 -0400

    add round_id to guesses table

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,6 +2,7 @@ class Guess < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :card
+  belongs_to :round
 
   validates :user_id, :card_id, :correct, :presence => true
   # Remember to create a migration!
--- app/models/round.rb
+++ app/models/round.rb
@@ -2,6 +2,7 @@ class Round < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :deck
+  has_many :guesses
 
   validates :user_id, :deck_id, :presence => true
 end
