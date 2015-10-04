310a926879b65d78f5d27ab38bfe5611a7b98346
maggiedbaker <maggiedbaker@gmail.com>
2015-10-03 22:57:15 -0400

    app now displays a new random card each time

--- app/controllers/guesses.rb
+++ app/controllers/guesses.rb
@@ -1,16 +1,24 @@
+# require 'pry'
+
 get '/decks/:deck_id/rounds/:round_id/guess' do |deck_id, round_id|
-  Guess.create(user_id: 1, round_id: round_id)
+  Guess.create(user_id: 1, round_id: round_id, card_id: 2, guess_text: "hallo")
   @round = Round.find(round_id)
   @deck = Deck.find(deck_id)
-  erb :'cards/show'
 
-  @next_card = @deck.cards.find { |card| card.guesses.select {|guess| guess.correct == true }.empty? }
+  @next_card = @deck.cards.shuffle.find { |card| card.guesses.select {|guess| guess.correct == true }.empty? }
 
   if @next_card == nil
     erb :'decks/index'
   else
     erb :'cards/show'
-    redirect "/decks/#{deck_id}/cards/#{next_card}"
   end
+  #   # redirect "/decks/#{deck_id}/cards/#{@next_card.id}"
 end
+
+post '/decks/:deck_id/rounds/:round_id/guess' do |deck_id, round_id|
+  "Hello World"
+end
+
+
+# Guess id: nil, user_id: 1, card_id: 2, correct: false, created_at: nil, updated_at: nil, guess_text: nil, round_id: nil>
 
